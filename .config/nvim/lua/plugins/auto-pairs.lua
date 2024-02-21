2f4ce102c57cbf70ce44290733adc74d4ce329d1
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2024-02-21 23:40:51 +0100

    Do not grab C-p in Markdown files

--- .config/nvim/lua/plugins/auto-pairs.lua
+++ .config/nvim/lua/plugins/auto-pairs.lua
@@ -2,6 +2,7 @@ return {
 	{ "jiangmiao/auto-pairs",
 		init = function()
 			vim.g.AutoPairsCenterLine = 0
+			vim.g.AutoPairsShortcutToggle = ""
 		end },
 	{ "tpope/vim-surround" },
 	{ "vim-scripts/matchit.zip" },
--- .config/nvim/lua/plugins/nvim-ide-markdown.lua
+++ .config/nvim/lua/plugins/nvim-ide-markdown.lua
@@ -5,6 +5,7 @@ return {
 		end },
 	{ "JamshedVesuna/vim-markdown-preview", ft = "markdown",
 		init = function()
+			vim.g.vim_markdown_preview_hotkey = "<leader>p"
 			vim.g.vim_markdown_preview_github = 1
 			vim.g.vim_markdown_preview_use_xdg_open = 1
 		end },
