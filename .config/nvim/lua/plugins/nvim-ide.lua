ccd60932cef5e7e41e52276677dec08e45c1f72d
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2024-10-23 15:43:21 +0200

    Run code in a REPL-like way

--- .config/nvim/lua/plugins/nvim-ide.lua
+++ .config/nvim/lua/plugins/nvim-ide.lua
@@ -10,8 +10,11 @@ return {
 
 	-- IDE-like session manager
 	{ "rmagatti/auto-session",
-		lazy = false, -- Load it on startup
-		opts = {} },
+		opts = { lazy_support = true },
+		init = function()
+			-- Add option recommended by the health check
+			vim.opt.sessionoptions = vim.opt.sessionoptions + "localoptions"
+		end },
 
 	{ "nvim-lualine/lualine.nvim",
 		dependencies = {
@@ -21,7 +24,7 @@ return {
 			sections = {
 				lualine_c = { "filename", { gps, cond = gps_is_available } },
 				lualine_x = { "copilot", "encoding", "filetype" } },
-			extensions = { "fugitive", "nvim-tree", "quickfix" },
+			extensions = { "fugitive", "man", "nvim-tree", "quickfix" },
 		} },
 
 	-- Breadcrumbs based on treesitter
@@ -30,7 +33,9 @@ return {
 
 	{ "nvim-tree/nvim-tree.lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		keys = {{ "<F2>", ":NvimTreeToggle<CR>", silent = true }},
+		keys = {
+			{ "<F2>", ":NvimTreeToggle<CR>",
+				silent = true, desc = "Toggle File Explorer" } },
 		opts = {
 			on_attach = function(buffer)
 				local api = require("nvim-tree.api")
@@ -41,11 +46,15 @@ return {
 		} },
 
 	{ "majutsushi/tagbar",
-		keys = {{ "<F8>", ":TagbarToggle<CR>", silent = true }} },
+		keys = {
+			{ "<F8>", ":TagbarToggle<CR>",
+				silent = true, desc = "Toggle TAG Explorer" } } },
 
 	-- Fuzzy file finder
 	{ "cloudhead/neovim-fuzzy",
-		keys = {{ "<C-p>", ":FuzzyOpen<CR>", silent = true }},
+		keys = {
+			{ "<C-p>", ":FuzzyOpen<CR>",
+				silent = true, desc = "Open Fuzzy File Finder" } },
 		init = function()
 			vim.g.fuzzy_hidden = 1
 		end },
@@ -57,14 +66,44 @@ return {
 	-- Switch between source and header
 	{ "derekwyatt/vim-fswitch",
 		lazy = false, -- Something is not right when lazy-loaded...
-		keys = {{ "<leader>h", ":FSHere<CR>", silent = true }} },
+		keys = {
+			{ "<leader>h", ":FSHere<CR>",
+				silent = true, desc = "Switch between source and header file" } } },
 
 	-- Comment-out code like a champ
 	{ "tpope/vim-commentary",
-		keys = {{ "<C-_>", ":Commentary<CR>", mode = { "n", "v" }, silent = true }} },
+		keys = {
+			{ "<C-_>", ":Commentary<CR>", mode = { "n", "v" },
+				silent = true, desc = "Toggle a comment" } } },
+
+	-- Prevent opening files in special windows
+	{ "stevearc/stickybuf.nvim",
+		opts = {} },
 
 	-- Open URLs with favorite browser
 	{ "tyru/open-browser.vim",
-		keys = {{ "<leader>g", "<Plug>(openbrowser-smart-search)", silent = true }} },
+		keys = {
+			{ "<leader>g", "<Plug>(openbrowser-smart-search)",
+				silent = true, desc = "Open/search current token in a web browser" } } },
+
+	-- Run code in a REPL-like way
+	{ "benlubas/molten-nvim",
+		build = ":UpdateRemotePlugins",
+		keys = {
+			{ "<leader>r", ":<C-u>MoltenEvaluateVisual<CR>gv", mode = { "v" },
+				silent = true, desc = "REPL: Evaluate visual selection" },
+			{ "<leader>rl", ":MoltenEvaluateLine<CR>",
+				silent = true, desc = "REPL: Evaluate line" },
+			{ "<leader>rr", ":MoltenReevaluateCell<CR>",
+				silent = true, desc = "REPL: Re-evaluate cell" },
+			{ "<leader>rd", ":MoltenDelete<CR>",
+				silent = true, desc = "REPL: Delete cell" },
+			{ "<leader>rc", ":MoltenInterrupt<CR>",
+				silent = true, desc = "REPL: Interrupt evaluation" },
+			{ "<leader>oh", ":MoltenHideOutput<CR>",
+				silent = true, desc = "REPL: Hide output" },
+			{ "<leader>os", ":noautocmd MoltenEnterOutput<CR>",
+				silent = true, desc = "REPL: Enter/show output" },
+		} },
 
 }
