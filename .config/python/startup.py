f19f5f8151609a55dd6a494dc66d2d7d432d3e89
Arkadiusz Bokowy <arkadiusz.bokowy@gmail.com>
2026-02-12 11:16:39 +0100

    Setup readline history for python < 3.13 only

--- .config/python/startup.py
+++ .config/python/startup.py
@@ -1,30 +1,29 @@
 # Startup script for python which enables history and tab name completion
 # for the interpreter interactive mode.
 
-# import needed modules
 import atexit
 import os
 import readline
 import rlcompleter
+import sys
 
-# where is history saved
-historyPath = os.path.expanduser("~/.python_history")
+if sys.version_info < (3, 13):
+    history = (os.getenv("PYTHON_HISTORY") or
+               os.path.expanduser("~/.python_history"))
+    # Handler for saving history.
+    def save_history(history=history):
+        import readline
+        readline.write_history_file(history)
+    # Read history, if it exists.
+    if os.path.exists(history):
+        readline.set_history_length(10000)
+        readline.read_history_file(history)
+    # Register saving handler.
+    atexit.register(save_history)
+    # Enable completion.
+    readline.parse_and_bind('tab: complete')
+    # Cleanup.
+    del save_history, history
 
-# handler for saving history
-def save_history(historyPath=historyPath):
-    import readline
-    readline.write_history_file(historyPath)
-
-# read history, if it exists
-if os.path.exists(historyPath):
-    readline.set_history_length(10000)
-    readline.read_history_file(historyPath)
-
-# register saving handler
-atexit.register(save_history)
-
-# enable completion
-readline.parse_and_bind('tab: complete')
-
-# cleanup
-del os, atexit, readline, rlcompleter, save_history, historyPath
+# Cleanup.
+del atexit, os, readline, rlcompleter, sys
